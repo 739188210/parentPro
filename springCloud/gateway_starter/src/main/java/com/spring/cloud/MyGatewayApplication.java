fadc06c25844ad1d14a8d594a41b50e8049ded2c
15277 <xin.miao@jereh.com>
2022-05-11 21:44:56 +0800

    update

---
+++ springCloud/gateway_starter/src/main/java/com/spring/cloud/MyGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: miao
+ * @date 2022/5/11
+ */
+@SpringBootApplication
+public class MyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGatewayApplication.class, args);
+    }
+}
